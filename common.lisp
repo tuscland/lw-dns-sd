73ab33f0e505c7da0b6ea9015410323d65f5071e
Camille Troillard <camille@osculator.net>
2012-02-25 00:10:59 +0100

    #'service-eq: function to compare services for equality by their name

--- common.lisp
+++ common.lisp
@@ -11,6 +11,14 @@
   (port 0)
   properties)
 
+
+(defun service-eq (service1 service2)
+  "Compares two services.  Two services are 'eq when they have the same name."
+  (and (service-p service1)
+       (service-p service2)
+       (string-equal (service-name service1)
+                     (service-name service2))))
+
 (defstruct record
   (interface-index 0)
   name
@@ -19,10 +27,12 @@
   data
   ttl)
 
+
 (defstruct domain
   (interface-index 0)
   name
   defaultp)
+
 
 (defclass handle ()
   ((ref
